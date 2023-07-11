dbdf7d6dbacaeeaf740e245935836d849f240953
krushnadevops <137031288+krushnadevops@users.noreply.github.com>
2023-07-11 13:21:17 +0530

    Final update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey , This is my aws devops project, created with the help of awsdevops getting started documentation. And its success.'));
-=======
+app.get('/', (req, res) => res.send('Whish You Happy Birthday TONY_IRON-HEART'));
+
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
