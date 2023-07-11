32aa5eaa196ffb08446f353cfb575ae07d14b336
krushna <krushnadevops008@gmail.com>
2023-07-11 12:49:23 +0530

    dont know

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+<<<<<<< HEAD
 app.get('/', (req, res) => res.send('Hey , This is my aws devops project, created with the help of awsdevops getting started documentation.'));
+=======
+app.get('/', (req, res) => res.send('Whish You Happy Birthday TONY_THE-IRON-HEART'));
+>>>>>>> parent of f975aae (modified once again)
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
