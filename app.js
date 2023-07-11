1a902f8c9dc816d694e9c5a337bdafd04a75cb2f
krushna <krushnadevops008@gmail.com>
2023-07-11 13:07:12 +0530

    success project2

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 app.get('/', (req, res) => res.send('Hey , This is my aws devops project, created with the help of awsdevops getting started documentation. And its success.'));
 =======
 app.listen(port);
